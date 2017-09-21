3a174b3af80cbd2304eba8a47fbecd5adf65d2a8
alex-klock <sombralobo13@gmail.com>
2017-09-20 23:06:13 -0400

    Added a spec bundle plugin... responsible for building up a spec bundle to execute spec context in both JIT and AOT modes.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './bundling/plugins/ng2-router-plugin';
+export * from './bundling/plugins/ng2-spec-bundle-plugin';
 export * from './bundling/plugins/ng2-template-plugin';
 export * from './analysis/components/component-context';
 export * from './analysis/components/component-info';
